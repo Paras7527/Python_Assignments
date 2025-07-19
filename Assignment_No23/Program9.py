48fed08351bf56719e7470dba501dd9abe1b0577
Paras Kulkarni <paraskulkarni7527@gmail.com>
2025-07-19 23:48:23 +0530

    Final commits of assignment23

--- Assignment_No23/Program9.py
+++ Assignment_No23/Program9.py
@@ -4,9 +4,27 @@
 #    'Math': [np.nan, 76, 88],
 #    'Science': [91, np.nan, 85]
 #}
+import pandas as pd
+import numpy as np
 
 def main():
-    pass
+    data2 = {
+    'Name': ['Amit', 'Sagar', 'Pooja'],
+    'Math': [np.nan, 76, 88],
+    'Science': [91, np.nan, 85]}
+
+    df = pd.DataFrame(data2)
+    print("The Dataframe is : \n",df)
+
+    #data2['Math'] = data2['Math'].fillna(data2['Math'].mean())
+
+    col_mean = df[['Math','Science']].mean()
+    print("Column mean : \n",col_mean)
+
+    df_Fill = df.fillna(col_mean)
+    print("The Updated Dataframe is : \n",df_Fill)
+
+    print(data2)
 
 if __name__ == "__main__":
     main()
